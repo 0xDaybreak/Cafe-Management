ea3c150521a8d5aa2af4f1f172244cf821b5797f
Deiu <andrei1.popescu1@gmail.com>
2022-05-15 22:42:32 +0300

    Made methods in CoffeeController.java public and wrote tests using JUnit 5 for them

---
+++ src/test/java/com/controller/CoffeeControllerTest.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import com.service.OrdersService;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoffeeControllerTest {
+
+    @Test
+    void checkIfDateIsTruncatedCorrectly() {
+        var coffeeController = new CoffeeController();
+        var date = new Date(1997, Calendar.MARCH, 13);
+        assertEquals(date,coffeeController.truncToSec(date));
+    }
+
+    @Test
+    void checkOrdersIdIsSetCorrectly() {
+        var ordersService = new OrdersService();
+        assertEquals(44,ordersService.findID().get(0).getOrdersId());
+
+    }
+
+}
